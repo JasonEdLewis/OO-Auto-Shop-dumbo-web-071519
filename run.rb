d5daa75a544dde88c8a5af754e017dbb7706c02e
Jason Lewis <jasonedlewis@gmail.com>
2019-07-22 16:01:04 +0000

    Automatically backed up by Learn

--- run.rb
+++ run.rb
@@ -1,6 +1,6 @@
 # require_relative '..app/models/car_owner.rb'
-require_relative '..app/models/car.rb'
-require_relative '..app/models/mechanic.rb'
+require_relative 'app/models/car.rb'
+require_relative 'app/models/mechanic.rb'
 
 require 'pry'
 
