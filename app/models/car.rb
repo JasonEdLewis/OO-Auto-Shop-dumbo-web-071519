660e981efdbe5ddbfff55cbf8945b8d69dbea205
Jason Lewis <jasonedlewis@gmail.com>
2019-07-22 16:08:40 +0000

    Automatically backed up by Learn

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,7 +2,7 @@ class Car
 
   attr_reader :make, :model
 
-  def initialize(make, model, classification)
+  def initialize(make, model, classification, car_owner, mechanic)
     @make = make
     @model = model
     @classification = classification
